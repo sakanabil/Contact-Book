6e3e6f16de84318b43594808e2bb1855234d687b
saka <sakanabil1221@gmail.com>
2025-04-18 18:53:59 +0700

    update migration table kontak

--- database/migrations/2025_04_18_115023_create_kontak_table.php
+++ database/migrations/2025_04_18_115023_create_kontak_table.php
@@ -4,24 +4,22 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateKontakTable extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+    public function up()
     {
         Schema::create('kontak', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            $table->id(); // id auto increment
+            $table->string('nama'); // nama kontak
+            $table->string('nomor_hp'); // nomor HP
+            $table->string('email')->nullable(); // email opsional
+            $table->text('alamat')->nullable(); // alamat opsional
+            $table->timestamps(); // created_at & updated_at
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('kontak');
     }
-};
+}
